a2448a958e90219b97e936cd0920d11447a71d55
kumar sashi <kumar.sashi@intimetec.com>
2020-12-07 11:06:27 +0530

    new commit

--- src/org/foo/gitcheckout.groovy
+++ src/org/foo/gitcheckout.groovy
@@ -1,7 +1,7 @@
 package org.foo
 
 def checkOutFrom(repo) {
-  git url: "git@github.com:https://github.com/javaparser/javaparser-maven-sample.git/${repo}"
+  git url: "git@github.com:https:javaparser/javaparser-maven-sample/${repo}"
 }
 
 return this
