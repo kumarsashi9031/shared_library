91a5e4db87174927bbbf71cf5d7b18de6cbcd55e
kumar sashi <kumar.sashi@intimetec.com>
2020-12-09 11:01:30 +0530

    newcommit

--- vars/notifySuccessful.groovy
+++ vars/notifySuccessful.groovy
@@ -1,5 +1,5 @@
 def call() {
     emailext body: '''$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS:
 
-Check console output at $BUILD_URL to view the results.''', compressLog: true, recipientProviders: [developers()], subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!', to: 'ak9031600@gmail.com' 'kumar.sashi@intimetec.com'
+Check console output at $BUILD_URL to view the results.''', compressLog: true, recipientProviders: [developers()], subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!', to: 'ak9031600@gmail.com'
 }
