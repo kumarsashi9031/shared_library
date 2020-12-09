136fa146156b8d6961035ea8e1303ab174b7ae6f
kumar sashi <kumar.sashi@intimetec.com>
2020-12-09 23:03:44 +0530

    origin master

--- src/org/foo/notifySuccessful.groovy
+++ src/org/foo/notifySuccessful.groovy
@@ -1,9 +1,9 @@
 package org.foo
 class notifySuccessful {
-  static def notify() {
+  static def notify(string args) {
      emailext body: '''$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS:
 
-      Check console output at $BUILD_URL to view the results.''', compressLog: true, recipientProviders: [developers()], subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!', to: 'ak9031600@gmail.com'
+      Check console output at $BUILD_URL to view the results.''', compressLog: true, recipientProviders: [developers()], subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!', to: "${args}"
    
     }
     
