3baa6c02d6f590fa048d9525cf81daa07dd26c1c
kumar sashi <kumar.sashi@intimetec.com>
2020-12-10 13:39:42 +0530

    new commit

--- src/org/foo/notifySuccessful.groovy
+++ src/org/foo/notifySuccessful.groovy
@@ -2,8 +2,8 @@ package org.foo
      def notify(subject,body,mail) {
           emailext (
     subject: "BUILD SUCCESSFULL:' ${subject} ${env.JOB_NAME} , BUILD NUMBER: [${env.BUILD_NUMBER}]'",
-    body: """STARTED: Job '${body} ${env.JOB_NAME} [${env.BUILD_NUMBER}]'
-     Check console output at URL='${env.BUILD_URL}' ${env.JOB_NAME} , BUILD NUMBER: [${env.BUILD_NUMBER}]""",
+    body: """'${body} the job is SUCCESSFULL with JOB NAME: ${env.JOB_NAME} and BUILD NUMBER: [${env.BUILD_NUMBER}]'
+     Check console output at URL='${env.BUILD_URL}' """,
     recipientProviders: [developers()] , to: "${mail}"
     )
       }
