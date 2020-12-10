bc9ad31d30323f302c9b08a2e3f42a6512208799
kumar sashi <kumar.sashi@intimetec.com>
2020-12-10 13:01:12 +0530

    new commit

--- src/org/foo/notifySuccessful.groovy
+++ src/org/foo/notifySuccessful.groovy
@@ -1,7 +1,7 @@
 package org.foo
      def notify(body,subject,mail) {
-           emailext body: "${body}"''' $PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS:
+           emailext body: '"${body}" $PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS:
 
-           Check console output at $BUILD_URL to view the results.''', compressLog: true, recipientProviders: [developers()], subject: "${subject}" ' $PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!', to: "${mail}"
+           Check console output at $BUILD_URL to view the results.''', compressLog: true, recipientProviders: [developers()], subject: '"${subject}"  $PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!', to: "${mail}"
       }
     return this
